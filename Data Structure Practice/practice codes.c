aff6f993935b8bbc041d9aa6b63e472b8532adbd
wudoophy_c <wu22023@outlook.com>
2024-09-24 22:21:50 +0800

    practice code

---
+++ Data Structure Practice/practice codes.c
@@ -0,0 +1,43 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<stdbool.h>
+
+//实现循环队列
+typedef struct CirQueue {
+	int* a;
+	int size;
+	int capacity;
+
+} CQue;
+
+CQue* MyCirQueue(int k ) {
+	int* pls = (int*)malloc(k * sizeof(int));
+
+}
+int Front() {
+
+}
+int Rear() {
+
+}
+bool enQueue() {
+
+}
+bool deQueue() {
+
+}
+void Empty() {
+
+
+}
+void Full() {
+
+
+}
+int main() {
+	CQue* head = NULL;
+
+
+
+	return 0;
+}
